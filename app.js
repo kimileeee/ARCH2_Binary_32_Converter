4e2fdc69b396338374a21b188d02d24a25030ad7
kimileeee <kim_leejra@dlsu.edu.ph>
2023-04-04 00:24:03 +0800

    added files for basic web app

---
+++ app.js
@@ -0,0 +1,18 @@
+// Imports
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+const port = 8080;
+
+app.use(express.static(__dirname + '/public'));
+app.use('/', router)
+
+app.listen(port, () => {
+    console.log('Listening on port ${port}')
+})
