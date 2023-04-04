1285af8d3d8d0cdf87afb3451fac15b58c0ee03c
Lauren Garcia <lauren_garcia@dlsu.edu.ph>
2023-04-05 02:25:20 +0800

    Convert to binary when base is 10

--- public/script.js
+++ public/script.js
@@ -1,28 +1,112 @@
 function clearInput(){
     document.getElementById("num").value="";
     document.getElementById("base").value="";
+    document.getElementById("exponent").value="";
     document.getElementById("binNum").value="";
     document.getElementById("hexNum").value="";
 }
 
-function decimal_to_binary(num){
-    console.log(num);
-    if(num == 0){
-        return [0];
-    }
-    let binary_integer = [];
-    while (num > 0){
-        binary_integer.splice(0, 0, num % 2);
+function integer_to_binary(num){
+    let binary = "";
+    while (num > 0) {
+        // if modulo of number with 2 is ‘1’, append 1 in front of binary string. Otherwise append 0.
+        if (num % 2 == 1) {
+           binary = "1" + binary;
+        } else {
+           binary = "0" + binary;
+        }
+        // divide number by 2.
         num = Math.floor(num / 2);
-        console.log(num);
+     }
+    return binary;
+}
+
+//accepts float
+function decimal_to_binary(num){
+    let dec = "";
+    var temp;
+
+    while(num % 1 != 0){
+        num = num * 2;
+        temp = Math.floor(num)
+        dec = dec + temp.toString();
     }
-    return binary_integer;
+    return dec;
+}
+
+
+function normalize(integer, decimal, exponent){
+    let binary = "";
+    var count = 0;
+
+    if(integer.toString().length == 1 && integer == "1")
+    {
+        binary = integer + "." + decimal;
+    }
+    else if (integer.toString().length > 1) { //shift decimal point to left
+        //var temp = parseInt(integer, 2);    
+        /*
+        while (temp != 1) {
+            temp = temp  >> 1;
+            count += 1;
+            var lastChar = temp.toString(2).slice(-1);
+            decimal = decimal.padStart(decimal.toString().length + 1, lastChar);
+        }*/
+        while (integer != "1") {
+            temp = integer.slice(-1);
+            count += 1;
+            decimal = decimal.padStart(decimal.toString().length + 1, temp);
+            integer = integer.slice(0, -1);
+            console.log("new dec: " + decimal);
+        }
+        binary = "1" + "." + decimal;
+        exponent = exponent + count;
+    }
+    else { //shift decimal point to right
+        var temp = decimal;
+    
+        while (temp.charAt(0) != "1") {
+            temp = temp.toString(2).slice(1);
+            count += 1;
+        }
+        binary = "1" + "." + temp;
+        exponent = exponent - count;
+    }
+
+    return [binary, exponent];
+}
+
+function get_sign(input){
+    if(input.includes('-')){
+        return '1';
+    }
+    else {return '0';}
+}
+
+function getFullMantissa(mantissa){
+    var length = mantissa.length;
+    var add = 23 - length;
+
+    for(let i =0; i < add; i++)
+    {
+        mantissa = mantissa + "0";
+    }
+
+    return mantissa;
 }
 
 
 function convert(){
     var input = document.getElementById("num").value;
-    var sign_bit;
+    var base = document.getElementById("base").value;
+    var exponent = document.getElementById("exponent").value;
+    var sign_bit = get_sign(input);
+    var splitNum = input.toString().split('.');
+    var integer = splitNum[0];
+    var dec  = "0." + splitNum[1];
+
+    if(dec == null)
+    {dec = "0";}
     
     if(input == "NaN"){
         binNum.innerHTML = '0 11111111 11111111111111111111111 (qNaN)';
@@ -31,11 +115,20 @@ function convert(){
     {
         binNum.innerHTML = '00000000000000000000000000000000';
     }
-    else if (input.includes('-'))
-    {
-        sign_bit = '1';
-        //var output = decimal_to_binary(12);
-        //console.log(output);
+    else if (base == "10"){
+       integer = integer_to_binary(parseInt(integer));
+       dec = decimal_to_binary(parseFloat(dec));
+
+       console.log(dec);
+
+       normalized_results = normalize(integer, dec, parseInt(exponent));
+       var binary = normalized_results[0];
+       exponent = 127 + normalized_results[1];
+       var expoRep = (exponent >>> 0).toString(2);
+
+       var answer = sign_bit + " " + expoRep + " " + getFullMantissa(binary.split('.')[1]);
+       binNum.innerHTML = answer;
+
     }
     else {
         binNum.innerHTML = '1';
