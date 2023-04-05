d9ab0c4f2a5069243f28bf86612dd5ba32007621
Lauren Garcia <lauren_garcia@dlsu.edu.ph>
2023-04-05 11:42:55 +0800

    Fixed mantissa length and normalization when shifting dec to right

--- public/script.js
+++ public/script.js
@@ -16,11 +16,18 @@ function integer_to_binary(num){
 function decimal_to_binary(num){
     let dec = "";
     var temp;
+    count = 0;
 
     while(num % 1 != 0){
         num = num * 2;
         temp = Math.floor(num)
+        if (temp >=1){
+            temp =1;
+            num = num - 1;
+        }
         dec = dec + temp.toString();
+        count += 1;
+        if(count == 31){break;};
     }
     return dec;
 }
@@ -43,19 +50,25 @@ function normalize_base10(integer, decimal, exponent){
             integer = integer.slice(0, -1);
         }
         binary = "1" + "." + decimal;
-        exponent = exponent + count;
+        exponent = count;
     }
-    else { //shift decimal point to right
+    else if (integer == 0) { //shift decimal point to right
+        console.log('int: ' + integer);
+        console.log('dec: ' + decimal);
         var temp = decimal;
     
         while (temp.charAt(0) != "1") {
             temp = temp.toString(2).slice(1);
             count += 1;
         }
+        count += 1;
+        temp = temp.toString(2).slice(1);
         binary = "1" + "." + temp;
-        exponent = exponent - count;
+        exponent = 0 - count;
+        console.log(decimal);
+        console.log(binary);
+
     }
-    
     return [binary, exponent];
 }
 
@@ -69,29 +82,52 @@ function get_sign(input){
 function getFullMantissa(mantissa){
     var length = mantissa.length;
     var add = 23 - length;
-
-    for(let i =0; i < add; i++)
-    {
-        mantissa = mantissa + "0";
+    
+    if (length < 23) {
+        for(let i =0; i < add; i++)
+        {
+            mantissa = mantissa + "0";
+        }
     }
-
+    if(length > 23) {
+        for(let i =length; i > 23; i--)
+        {
+            mantissa = mantissa.slice(0, -1);
+        }   
+    }
+    console.log(length);
     return mantissa;
 }
 
+//adds leading zeros
+function getFullExponent(num){
+    var length = num.length;
+    var add = 8 - length;
+
+    for(let i =0; i < add; i++)
+    {
+        num = num.padStart(num.toString().length + 1, "0");
+    }
+    return num;
+}
 
 function convert(){
     var input = document.getElementById("num").value;
     var base = document.getElementById("base").value;
     var exponent = document.getElementById("exponent").value;
     var sign_bit = get_sign(input);
-
     var splitNum = input.toString().split('.');
 
     var integer = parseFloat(splitNum[0]);
     var dec  = parseFloat("0." + splitNum[1]);
 
-    input = integer + dec;
-    input = parseFloat(input) * parseFloat(Math.pow(10, exponent));
+    /*
+    dec = parseFloat(dec) * parseFloat(Math.pow(10.0, exponent));
+    if(dec == 0){dec = ".0";}
+
+    input = parseFloat(integer.toString() + dec.toString());
+    */
+   input = parseFloat(integer+dec) * parseFloat(Math.pow(10.0, exponent))
     
     splitNum = input.toString().split('.');
     integer = parseFloat(splitNum[0]);
@@ -99,6 +135,8 @@ function convert(){
 
     if(dec == null)
     {dec = "0";}
+    console.log('input: '+ input);
+    
     
     if(input == "NaN"){
         binNum.innerHTML = '0 11111111 11111111111111111111111 (qNaN)';
@@ -108,18 +146,15 @@ function convert(){
         binNum.innerHTML = '00000000000000000000000000000000';
     }
     else if (base == "10"){
-       
        integer = integer_to_binary(parseInt(integer));
        dec = decimal_to_binary(parseFloat(dec));
-
-       console.log(dec);
 
        normalized_results = normalize_base10(integer, dec, parseInt(exponent));
        var binary = normalized_results[0];
        exponent = 127 + normalized_results[1];
-       var expoRep = (exponent >>> 0).toString(2);
+       var expoRep = integer_to_binary(parseInt(exponent))
 
-       var answer = sign_bit + " " + expoRep + " " + getFullMantissa(binary.split('.')[1]);
+       var answer = sign_bit + " " + getFullExponent(expoRep) + " " + getFullMantissa(binary.split('.')[1]);
        binNum.innerHTML = answer;
     }
     else {
