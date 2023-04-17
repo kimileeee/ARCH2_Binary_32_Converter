2e58dfc890210bc6383f54152b99a3c02c571d6e
kimileeee <kim_leejra@dlsu.edu.ph>
2023-04-17 14:59:33 +0800

    fix

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 // Imports
 const express = require('express');
 const path = require('path');
-const product = require('./api/product');
+const router = express.Router();
 
 const app = express();
 
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+    // res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+});
+
 const port = process.env.PORT || 3000;
 
-app.use('/api/product', product);
 app.use('/public', express.static(__dirname + '/public'));
+app.use('/', router);
 
 app.listen(port, () => {
     console.log('Listening on port ' + port)
-})
+});
